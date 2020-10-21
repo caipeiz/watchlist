2d41ef5065e5ab52ac60387561ccce989d303d69
towngas <caipei1028@qq.com>
2020-10-21 17:00:55 +0800

    git commit -m "git pycharm test"

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ movies = [
     {'title': 'Dead Poets Society', 'year': '1989'},
     {'title': 'A Perfect World', 'year': '1993'},
     {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'I Love You', 'year': '1994'},
     {'title': 'Swallowtail Butterfly', 'year': '1996'},
     {'title': 'King of Comedy', 'year': '1999'},
     {'title': 'Devils on the Doorstep', 'year': '1999'},
